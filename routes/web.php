f7423a6d5c734bfabd81e7642d0f0a4f748f4740
Franco C Sanchez <francocsanchez@gmail.com>
2021-09-22 21:16:02 -0300

    Create MVC Message

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('message/{message}',[MessageController::class,'show'])->name('message.show');
+Route::post('message',[MessageController::class,'store'])->name('message.store');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
