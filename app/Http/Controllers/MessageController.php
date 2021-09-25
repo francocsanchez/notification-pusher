dbf579e7e4bdeb905f23ca99d04f7d3b47910008
Franco C Sanchez <francocsanchez@gmail.com>
2021-09-25 13:00:09 -0300

    Create notification mail

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Message;
-
+use App\Models\User;
+use App\Notifications\MessageSent;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
@@ -21,12 +22,15 @@ class MessageController extends Controller
             'to_user_id'    =>  'required|exists:users,id',
         ]);
 
-        Message::create([
+        $message = Message::create([
             'subject' => $request->subject,
             'body'  =>  $request->body,
             'from_user_id'  =>  auth()->id(),
             'to_user_id'    =>  $request->to_user_id
         ]);
+
+        $user = User::find($request->to_user_id);
+        $user->notify(new MessageSent($message));
 
         $request->session()->flash('flash.banner', 'Tu mensaje fue enviado correctamente');
         $request->session()->flash('flash.bannerStyle', 'success');
